c4118c92d17125fc3fd52c8961f0402f1722ccc9
Marko Raatikka <marko@thinglink.com>
2012-11-09 20:38:45 +0200

    Did more stuff

--- src/XMPExtractor.java
+++ src/XMPExtractor.java
@@ -3,26 +3,28 @@ import java.io.IOException;
 
 import org.apache.sanselan.ImageReadException;
 import org.apache.sanselan.Sanselan;
-import org.apache.sanselan.common.IImageMetadata;
-import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
+
+import com.adobe.xmp.XMPException;
+import com.adobe.xmp.XMPMeta;
+import com.adobe.xmp.XMPMetaFactory;
+import com.adobe.xmp.XMPSchemaRegistry;
 
 
 public class XMPExtractor {
     private static File file;
-    private static JpegImageMetadata metadata;
+    private static XMPSchemaRegistry registry = XMPMetaFactory.getSchemaRegistry();
     
     public XMPExtractor(File file) {
         this.file = file;
     }
     
-    public void extractMetaData() throws ImageReadException, IOException {
-        IImageMetadata temp = Sanselan.getMetadata(this.file);
-        if(temp instanceof JpegImageMetadata)
-        {
-            this.metadata = (JpegImageMetadata)temp;
-            this.metadata.getRawImageData().getImageData();
-        }
-        System.out.println(Sanselan.getXmpXml(file));
+    public void extractMetaData() throws ImageReadException, IOException, XMPException {
+
+        String text = Sanselan.getXmpXml(file);
+        System.out.println(text);
+        XMPMeta meta = XMPMetaFactory.parseFromString(text);
+        registry.registerNamespace("ns:meta/", "adobe");
+        meta.getPropertyString("adobe:ns:meta/", "dc:identifier");
         
     }
     
